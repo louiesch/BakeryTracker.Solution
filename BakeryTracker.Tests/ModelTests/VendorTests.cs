20afdc5cde735ab1f88cc95721ac8ddb8335846c
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-05 13:03:34 -0800

    write and pass first vendor test to create instance of vendor

---
+++ BakeryTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace BakeryTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test name", "test description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+  }
+}
--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -1,10 +1,21 @@
 using System.Collections.Generic;
-using System;
 
 namespace BakeryTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _vendorInstances = new List<Vendor> {};
+    public string VendorName { get; set; }
+    public string VendorDescription { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
 
+    public Vendor(string vendorName, string vendorDescription)
+    {
+      VendorName = vendorName;
+      VendorDescription = vendorDescription;
+      _vendorInstances.Add(this);
+      Orders = new List<Order>{};
+    }
   }
 }
