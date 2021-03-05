3acec20de8d8ebb9ce88f7ddfa29783c2c559684
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-05 15:05:50 -0800

    add controller path and view to display all orders a vendor has placed, edit orders/show page

--- BakeryTracker/Controllers/VendorsController.cs
+++ BakeryTracker/Controllers/VendorsController.cs
@@ -20,6 +20,24 @@ namespace BakeryTracker.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
     
   }
 }
